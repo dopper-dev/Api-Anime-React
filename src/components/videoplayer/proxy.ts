c73483e9e43423ace19d3e8e27c5f3b9f8394a02
Dopper <tbtumbal123@gmail.com>
2023-12-02 18:10:08 +0700

    FILE PROJECTNYA

---
+++ src/components/videoplayer/proxy.ts
@@ -0,0 +1,8 @@
+export const proxy = {
+  chayce: "https://proxy.anistreme.live/",
+  mine: "https://simple-cors-proxy-llxtxgrsbq-uc.a.run.app/",
+  mine2: "https://cors-anywhere.herokuapp.com/",
+  mine3: "https://cors.bridged.cc/",
+  mine4: "https://cors-anywhere-llxtxgrsbq-uc.a.run.app/",
+  cors: "https://proxy.cors.sh/",
+};
